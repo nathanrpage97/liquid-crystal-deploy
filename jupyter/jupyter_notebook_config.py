ea1f9f31c1352eb9ab3a1d6ad3e6f4784d1436b4
Nathan Page <nathanrpage97@gmail.com>
2019-11-20 21:21:13 -0500

    feat: seperate configuration of jupyter notebook from dockerfile

---
+++ jupyter/jupyter_notebook_config.py
@@ -0,0 +1,11 @@
+import os
+from IPython.lib import passwd
+c = get_config()
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 8080
+c.NotebookApp.open_browser = False
+c.NotebookApp.allow_root=True
+c.NotebookApp.token=''
+c.NotebookApp.password=passwd('liquid_crystal')
+c.NotebookApp.allow_password_change=False
+c.NotebookApp.base_url='/notebook/'
